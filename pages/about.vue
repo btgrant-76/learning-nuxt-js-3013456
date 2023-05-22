17fd160461e25cb79c5367e14239ab8745830a1a
Brian Grant <brian@skeletonjack.net>
2023-05-22 08:47:17 -0500

    01.06 challenge

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>About Me</h1>
+    <h2>Professional</h2>
+    <p>I work a Morningstar as a software developer.</p>
+    <h2>Hobbies</h2>
+    <p>I like keybaords</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style>
+body {
+  border: 8px solid salmon;
+  margin: 0;
+  height: 100vh;
+
+}
+.container {
+  text-align: center;
+  font-size: 30px;
+}
+</style>
